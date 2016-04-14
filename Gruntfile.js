7c51fa89a77480f39204391c874320a7160e1ff2
Thiago Lagden <lagden@gmail.com>
2016-04-14 19:20:15 -0300

    simplificando

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,92 +1,100 @@
-module.exports = function(grunt) {
+module.exports = grunt => {
+	const config = {
+		// Clean folders
+		clean: {
+			build: ['build/vanilla-masker*']
+		},
 
-  var config = {
+		// Concat
+		concat: {
+			options: {
+				separator: ";"
+			},
+			build: {
+				src: ["lib/vanilla-masker.js"],
+				dest: "build/vanilla-masker.min.js"
+			}
+		},
 
-    // Clean folders ================================
-    clean: {
-      dev: ['public/vanilla-masker*'],
-      build: ['build/vanilla-masker*']
-    },
+		// Jasmine Runner
+		jasmine: {
+			dev: {
+				src: ['lib/vanilla-masker.js'],
+				options: {
+					specs: 'test/*_spec.js'
+				}
+			}
+		},
 
-    // Concat ========================================
-    concat: {
-      options: {
-        separator: ";"
-      },
-      dev: {
-        src: ["lib/vanilla-masker.js"],
-        dest: "public/vanilla-masker.js"
-      },
-      build: {
-        src: ["lib/vanilla-masker.js"],
-        dest: "build/vanilla-masker.min.js"
-      }
-    },
+		// JSHint
+		jshint: {
+			all: ["lib/vanilla-masker.js"]
+		},
 
-    // Jasmine Runner ================================
-    jasmine: {
-      dev: {
-        src: ['lib/vanilla-masker.js'],
-        options: {
-          specs: 'tests/*_spec.js'
-        }
-      }
-    },
+		// Minification
+		uglify: {
+			minify: {
+				files: {
+					"build/vanilla-masker.min.js": ["build/vanilla-masker.min.js"]
+				}
+			}
+		},
 
-    // JSHint ========================================
-    jshint: {
-      all: ["lib/vanilla-masker.js"]
-    },
+		// Compress
+		compress: {
+			build: {
+				options: {
+					mode: 'gzip',
+					level: 9,
+					pretty: true
+				},
+				files: [
+					{
+						expand: true,
+						flatten: true,
+						src: ['build/*.js'],
+						dest: 'build',
+						ext: '.min.js.gz'
+					}
+				]
+			}
+		},
 
-    // Minification ==================================
-    uglify: {
-      minify: {
-        files: {
-          "build/vanilla-masker.min.js": ["build/vanilla-masker.min.js"]
-        }
-      }
-    },
+		// Connect Server
+		connect: {
+			dev: {
+				options: {
+					port: 4500,
+					base: 'public'
+				}
+			}
+		},
 
-    // Compress ======================================
-    compress: {
-      build: {
-        options: { mode: 'gzip', level: 9, pretty: true },
-        files: [
-          {expand: true, flatten: true, src: ['build/*.js'], dest: 'build', ext: '.min.js.gz'}
-        ]
-      }
-    },
+		// Watch Files
+		watch: {
+			livereload: {
+				options: {
+					livereload: true
+				},
+				files: ['lib/*', 'test/*', 'Gruntfile.js', 'package.json'],
+				tasks: ['default']
+			}
+		}
 
-    // Connect Server ================================
-    connect: {
-      dev: {
-        options: { port: 4500, base: 'public' }
-      }
-    },
+	};
 
-    // Watch Files ===================================
-    watch: {
-      livereload: {
-        options: { livereload: true },
-        files: ['lib/*', 'tests/*', 'Gruntfile.js', 'package.json'],
-        tasks: ['default']
-      }
-    }
+	grunt.initConfig(config);
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-jasmine");
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks("grunt-contrib-watch");
 
-  };
-
-  grunt.initConfig(config);
-  grunt.loadNpmTasks("grunt-contrib-clean");
-  grunt.loadNpmTasks("grunt-contrib-concat");
-  grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-contrib-uglify");
-  grunt.loadNpmTasks("grunt-contrib-jasmine");
-  grunt.loadNpmTasks('grunt-contrib-compress');
-  grunt.loadNpmTasks('grunt-contrib-connect');
-  grunt.loadNpmTasks("grunt-contrib-watch");
-
-  grunt.registerTask("default", ["jshint", "jasmine:dev"]);
-  grunt.registerTask("test", ["default"]);
-  grunt.registerTask("dev", ["default", "clean:dev", "concat:dev", "connect", "watch"]);
-  grunt.registerTask("build", ["default", "clean:build", "concat:build", "uglify", "compress"]);
+	grunt.registerTask("default", ["jshint", "jasmine:dev"]);
+	grunt.registerTask("test", ["default"]);
+	grunt.registerTask("dev", ["default", "clean:dev", "concat:dev", "connect", "watch"]);
+	grunt.registerTask("build", ["default", "clean:build", "concat:build", "uglify", "compress"]);
 };
